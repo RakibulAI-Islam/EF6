586a4a9b2343a1a9309a5cfa7217bc73e81788cb
Rakibul_AI <rakib.ice07@gmail.com>
2025-01-27 17:28:49 +0600

    12.'Re_name' the 'App_DbContext.cs' to 'Inventory_DbContext.cs'.

--- Chapter-3/EF6_0302/1_EF_DbLibrary/1_EF_DbLibrary/Inventory_DbContext.cs
+++ Chapter-3/EF6_0302/1_EF_DbLibrary/1_EF_DbLibrary/Inventory_DbContext.cs
@@ -2,10 +2,10 @@
 
 namespace _1_EF_DbLibrary
 {
-    public class App_DbContext : DbContext
+    public class Inventory_DbContext : DbContext
     {
-        public App_DbContext() { } //For Scaffolding.
-        public App_DbContext( DbContextOptions options) : base( options) //For Dependency Injection.
+        public Inventory_DbContext() { } //For Scaffolding.
+        public Inventory_DbContext( DbContextOptions options) : base( options) //For Dependency Injection.
         { }
         /*public AdventureWorks_2022Context(DbContextOptions<AdventureWorks_2022Context> options)
             : base(options)
