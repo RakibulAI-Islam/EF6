b2be866b73731c51c6b13d11cacfc9646640f4dc
Rakibul_AI <rakib.ice07@gmail.com>
2025-01-28 16:07:19 +0600

    18.'Add' the 'DbSet<Item>' _property to the 'Inventory_DbContext.cs' _class of '1_EF_DbLibrary' _project.

--- Chapter-3/EF6_0302/1_EF_DbLibrary/1_EF_DbLibrary/Inventory_DbContext.cs
+++ Chapter-3/EF6_0302/1_EF_DbLibrary/1_EF_DbLibrary/Inventory_DbContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Inventory_Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace _1_EF_DbLibrary
 {
     public class Inventory_DbContext : DbContext
     {
+        public DbSet<Item> Items { get; set; }
+        
         public Inventory_DbContext() { } //For Scaffolding.
+        
         public Inventory_DbContext( DbContextOptions options) : base( options) //For Dependency Injection.
         { }
+        
         /*public AdventureWorks_2022Context(DbContextOptions<AdventureWorks_2022Context> options)
             : base(options)
         {
