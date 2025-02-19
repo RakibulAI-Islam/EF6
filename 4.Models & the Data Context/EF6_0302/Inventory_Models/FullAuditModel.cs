0a89328970fd1d868de78e0b3da5955c49a0a1c2
Rakibul_AI <rakib.ice07@gmail.com>
2025-02-20 03:09:19 +0600

    29.'Inherit' the 'FullAuditModel' _class from the Interfaces.

--- 4.Models & the Data Context/EF6_0302/Inventory_Models/FullAuditModel.cs
+++ 4.Models & the Data Context/EF6_0302/Inventory_Models/FullAuditModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inventory_Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Inventory_Models
 {
-    public abstract class FullAuditModel
+    public abstract class FullAuditModel : IIdentityModel, IAuditedModel, IActivatableModel
     {
+        public int Id { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedUserId { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
