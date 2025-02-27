9db9ac38f28d6812361e16670d2bf8257001b8fb
Rakibul_AI <rakib.ice07@gmail.com>
2025-02-28 00:02:57 +0600

    2.'Add' the 'EFCore_Activity04-1_StarterFiles' _file.

---
+++ 4.Models & the Data Context/EFCore_Activity04-1_StarterFiles/EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,39 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+
+        public DbSet<Item> Items { get; set; }
+
+        //Add a default constructor if scaffolding is needed
+        public InventoryDbContext() { }
+
+        //Add the complex constructor for allowing Dependency Injection
+        public InventoryDbContext(DbContextOptions options)
+            : base(options)
+        {
+            //intentionally empty. 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+    }
+}
