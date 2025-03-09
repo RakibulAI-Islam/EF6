37f47398fb3a45f81d33ccb122d20f28b38920da
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-09 09:56:25 +0600

    29.'Add' properties of 'FullAuditModel.cs' _public Abstract class.

--- 4.Models & the Data Context/EFCore_Activity04-1_StarterFiles/InventoryModels/FullAuditModel.cs
+++ 4.Models & the Data Context/EFCore_Activity04-1_StarterFiles/InventoryModels/FullAuditModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Inventory_Models.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Inventory_Models
 {
-    public abstract class FullAuditModel
+    public abstract class FullAuditModel : IIdentityModel, IAuditedModel, IActivatableModel
     {
+        public int Id { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedUserId { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
