28449a9d401bc754dea9ee65e98b43a8a24041c2
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-09 19:13:23 +0600

    1.'Copy' the 'EFCore_Activity04-2_StarterFiles.zip' & 'EFCore_Activity04-2_StarterFiles'.

---
+++ 4.Models & the Data Context/EFCore_Activity04-2_StarterFiles/EFCore_Activity0402/Program.cs
@@ -0,0 +1,67 @@
+﻿using EFCore_DBLibrary;
+using InventoryHelpers;
+using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace EFCore_Activity0402
+{
+    public class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
+        static void Main(string[] args)
+        {
+            BuildOptions();
+            EnsureItems();
+            ListInventory();
+        }
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+
+        static void EnsureItems()
+        {
+            EnsureItem("Batman Begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember the Titans");
+            EnsureItem("Star Wars: The Empire Strikes Back");
+            EnsureItem("Top Gun");
+        }
+
+        private static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                //determine if item exists:
+                var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower()
+                                                            == name.ToLower());
+
+                if (existingItem == null)
+                {
+                    //doesn't exist, add it.
+                    var item = new Item() { Name = name };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Items.OrderBy(x => x.Name).ToList();
+                items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+            }
+        }
+
+    }
+}
---
+++ 4.Models & the Data Context/EFCore_Activity04-2_StarterFiles/InventoryModels/FullAuditModel.cs
@@ -0,0 +1,16 @@
+﻿using InventoryModels.Interfaces;
+using System;
+
+namespace InventoryModels
+{
+    public abstract class FullAuditModel : IIdentityModel, IAuditedModel, IActivatableModel
+    {
+        public int Id { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedUserId { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+}
