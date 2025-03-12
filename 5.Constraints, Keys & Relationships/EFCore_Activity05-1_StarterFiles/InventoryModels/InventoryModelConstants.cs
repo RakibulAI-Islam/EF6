b9cd7cc31fc343aa27738c9e74e6aa5805a7cf0b
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-12 10:58:38 +0600

    7.'Write' 'public const int MAX_DESCRIPTION_LENGTH = 250;' ,s.

--- 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/InventoryModelConstants.cs
+++ 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/InventoryModelConstants.cs
@@ -8,5 +8,9 @@ namespace InventoryModels
 {
     public class InventoryModelConstants
     {
+        public const int MAX_DESCRIPTION_LENGTH = 250;
+        public const int MAX_NAME_LENGTH = 100;
+        public const int MAX_NOTES_LENGTH = 2000;
+        public const int MAX_USERID_LENGTH = 50;
     }
 }
