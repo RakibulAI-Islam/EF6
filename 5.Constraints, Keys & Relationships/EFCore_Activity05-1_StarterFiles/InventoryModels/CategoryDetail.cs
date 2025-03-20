cf3547a32be16a5961ff5af6d79c4ae8aea8a700
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-20 14:07:49 +0600

    30.1.'Inherit from IIdentityModel' & 2.'Add' 'Properties' of 'CategoryDetail.cs' _class in 'InventoryModels' _project.

--- 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/CategoryDetail.cs
+++ 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/CategoryDetail.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InventoryModels.Interfaces;
 
 namespace InventoryModels
 {
-    public class CategoryDetail
+    public class CategoryDetail : IIdentityModel
     {
+        [Key, ForeignKey("Category")]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(InventoryModelsConstants.MAX_COLORVALUE_LENGTH)]
+        public string ColorValue { get; set; }
+
+        [Required]
+        [StringLength(InventoryModelsConstants.MAX_COLORNAME_LENGTH)]
+        public string ColorName { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
