8af16e524102d6a559addfa82267ae85bec697c5
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-21 13:09:21 +0600

    38.'Add' 'Properties' of 'Player.cs' in 'InventoryModels' _project.

--- 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/Player.cs
+++ 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace InventoryModels
 {
     public class Player : FullAuditModel
     {
+        [Required]
+        [StringLength(InventoryModelsConstants.MAX_PLAYERNAME_LENGTH)]
+        public string Name { get; set; }
+
+        [StringLength(InventoryModelsConstants.MAX_PLAYERDESCRIPTION_LENGTH)]
+        public string Description { get; set; }
+
+        public virtual List<Item> Items { get; set; } = new List<Item>();
     }
 }
