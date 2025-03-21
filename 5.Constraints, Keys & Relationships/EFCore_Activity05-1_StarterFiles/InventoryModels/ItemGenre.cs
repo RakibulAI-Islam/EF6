54dae4d83ecf73dbe986f350348a2e38e45d62b3
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-21 19:36:40 +0600

    54.'Add' the Properties of 'ItemGenre.cs' _class in 'InventoryModels' _project.

--- 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/ItemGenre.cs
+++ 5.Constraints, Keys & Relationships/EFCore_Activity05-1_StarterFiles/InventoryModels/ItemGenre.cs
@@ -9,5 +9,12 @@ namespace InventoryModels
 {
     public class ItemGenre : IIdentityModel
     {
+        public int Id { get; set; }
+
+        public virtual int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+
+        public virtual int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
     }
 }
