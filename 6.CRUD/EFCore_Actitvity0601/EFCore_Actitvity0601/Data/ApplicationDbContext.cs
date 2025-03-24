086b477bc973396726a7b91584838dfb3480d5a4
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-24 22:19:58 +0600

    10.'Add' 'DbSet<TEntity>' _Properties in 'ApplicationDbContext.cs' _class in 'Main' _project.

--- 6.CRUD/EFCore_Actitvity0601/EFCore_Actitvity0601/Data/ApplicationDbContext.cs
+++ 6.CRUD/EFCore_Actitvity0601/EFCore_Actitvity0601/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using InventoryModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCore_Actitvity0601.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryDetail> CategoryDetails { get; set; }
+        public DbSet<Genre> Genres { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
