8d300806a14d1ed889e3f1382e7f3fd44db166a3
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-24 18:41:20 +0600

    4.'Butify' the 'Program.cs'.

--- 6.CRUD/EFCore_Actitvity0601/EFCore_Actitvity0601/Program.cs
+++ 6.CRUD/EFCore_Actitvity0601/EFCore_Actitvity0601/Program.cs
@@ -3,15 +3,18 @@ using EFCore_Actitvity0601.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
