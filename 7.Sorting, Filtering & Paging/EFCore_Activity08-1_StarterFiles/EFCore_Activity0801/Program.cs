9cf0d160b6d424abec345456f3ef4cc7aed67ad3
Rakibul_AI <rakib.ice07@gmail.com>
2025-03-30 15:25:21 +0600

    1.'Copy' the 'EFCore_Activity08-1_StarterFiles' from 'github' in '7.Sorting, Filtering & Paging' _folder.

---
+++ 7.Sorting, Filtering & Paging/EFCore_Activity08-1_StarterFiles/EFCore_Activity0801/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EFCore_DBLibrary;
+using InventoryHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace EFCore_Activity0801
+{
+    class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
+
+        static void Main(string[] args)
+        {
+            BuildOptions();
+        }
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+        }
+    }
+}
+
+
